26d33631ec1c1a841cf57c3bf61f85c107d65a8b
hanyi <2387870957@qq.com>
2022-08-07 16:09:29 +0800

    重复三次布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,13 +15,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .nav-wrapper{
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-  }
-  .content{
-    overflow: auto;
-    flex-grow: 1;
-  }
+.nav-wrapper {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
+.content {
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Statistics.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Statistics.vue
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +15,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-wrapper {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 
+.content {
+  overflow: auto;
+  flex-grow: 1;
+}
 </style>
