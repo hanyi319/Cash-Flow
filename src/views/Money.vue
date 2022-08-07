dafcf6818785095db1f5c8397b0dd7df9627f840
hanyi <2387870957@qq.com>
2022-08-07 11:55:36 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Money.vue
+  <div class="nav-wrapper">
+    <div class="content">
+      Money.vue
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +15,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .nav-wrapper{
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content{
+    overflow: auto;
+    flex-grow: 1;
+  }
 </style>
