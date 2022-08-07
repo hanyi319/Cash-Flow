85ffb2cc1bf96d0103772a9e73761d55feddcfab
hanyi <2387870957@qq.com>
2022-08-08 00:24:02 +0800

    修改导航栏样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -23,12 +23,13 @@ export default {
 
 <style lang="scss" scoped>
   nav{
+    padding: 0;
     display: flex;
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
     flex-direction: row;
     font-size: 12px;
     > .item{
-      padding: 2px 0;
+      padding: 5px 0;
       width: 33.33333%;
       display: flex;
       flex-direction: column;
