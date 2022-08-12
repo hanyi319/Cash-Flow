522cc18bce932b3a0d2b2044c2ee30a39b4e95b6
hanyi <2387870957@qq.com>
2022-08-12 23:47:01 +0800

    收集记账页四个组件的数据到 record

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="numberPad">
-    <div class="output">{{output || '&nbsp;'}}</div>
+    <div class="output">{{ output || '&nbsp;' }}</div>
     <div class="buttons">
       <button @click="inputContent">1</button>
       <button @click="inputContent">2</button>
@@ -27,11 +27,12 @@ import {Component, Prop} from 'vue-property-decorator';
 @Component
 export default class NumberPad extends Vue {
   output: string = '0';
+
   inputContent(event: MouseEvent) {
     const button = (event.target as HTMLButtonElement);
     const input = button.textContent as string;
-    if(this.output.length === 16) { return; }
-    if(this.output === '0') {
+    if (this.output.length === 16) { return; }
+    if (this.output === '0') {
       if ('0123456789'.indexOf(input) >= 0) {
         this.output = input;
       } else {
@@ -39,25 +40,31 @@ export default class NumberPad extends Vue {
       }
       return;
     }
-    if(this.output.indexOf('.') >= 0 && input === '.') { return; }
+    if (this.output.indexOf('.') >= 0 && input === '.') { return; }
     this.output += input;
   }
+
   remove() {
-    if(this.output.length === 1) {
+    if (this.output.length === 1) {
       this.output = '0';
     } else {
       this.output = this.output.slice(0, -1);
     }
   }
+
   clear() {
     this.output = '0';
   }
-  ok() {}
+
+  ok() {
+    this.$emit('update:value', this.output);
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
+
 .numberPad {
   .output {
     @extend %clearFix;
@@ -68,9 +75,11 @@ export default class NumberPad extends Vue {
     padding: 9px 16px;
     text-align: right;
   }
+
   .buttons {
     @extend %clearFix;
     background: #f1f1f1;
+
     > button {
       width: 25%;
       height: 64px;
@@ -81,12 +90,14 @@ export default class NumberPad extends Vue {
       font-weight: bold;
       background: #ffffff;
       border: 4px solid #f1f1f1;
+
       &.ok {
         background: #2AAE67;
         color: #ffffff;
         height: 64*2px;
         float: right;
       }
+
       &.zero {
         width: 25*2%;
       }
