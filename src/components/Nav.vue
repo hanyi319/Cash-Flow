78b5f4158a6ccfa1786fdd78357641a8eee2eabe
hanyi <2387870957@qq.com>
2022-08-15 18:57:52 +0800

    点击标签可跳转至编辑页面

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,5 @@
 <template>
   <nav>
-    <router-link to="/labels" class="item" active-class="selected">
-      <Icon name="label"/>
-      标签
-    </router-link>
     <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
@@ -11,6 +7,10 @@
     <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
+    </router-link>
+    <router-link to="/labels" class="item" active-class="selected">
+      <Icon name="label"/>
+      标签
     </router-link>
   </nav>
 </template>
