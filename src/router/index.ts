a6544ec14071eef445b6c1218308c21b1cbdae88
hanyi <2387870957@qq.com>
2022-08-15 18:07:37 +0800

    将 tag 的 string 改为 {id: string, name: string}

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,42 @@
-import Vue from 'vue';
-import VueRouter, {RouteConfig} from 'vue-router';
-import Money from '@/views/Money.vue';
-import Labels from '@/views/Labels.vue';
-import Statistics from '@/views/Statistics.vue';
-import NotFound from '@/views/NotFound.vue';
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Money from "@/views/Money.vue";
+import Labels from "@/views/Labels.vue";
+import Statistics from "@/views/Statistics.vue";
+import NotFound from "@/views/NotFound.vue";
+import EditLabel from "@/views/EditLabel.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    redirect: '/money'
+    path: "/",
+    redirect: "/money",
   },
   {
-    path: '/money',
-    component: Money
+    path: "/money",
+    component: Money,
   },
   {
-    path: '/labels',
-    component: Labels
+    path: "/labels",
+    component: Labels,
   },
   {
-    path: '/statistics',
-    component: Statistics
+    path: "/labels/edit",
+    component: EditLabel,
   },
   {
-    path: '*',
-    component: NotFound
-  }
+    path: "/statistics",
+    component: Statistics,
+  },
+  {
+    path: "*",
+    component: NotFound,
+  },
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
 });
 
 export default router;
