10f4698dadb3eea392a03959e1989c92a9d35020
韩毅 <2387870957@qq.com>
2022-08-20 12:47:56 +0800

    所有组件成功使用 Vuex 进行数据管理

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,7 +6,7 @@
       <span class="rightIcon"/>
     </div>
     <div class="form-wrapper">
-      <FormItem :value="tag.name" @update:value="update" field-name="标签名：" placeholder="请输入标签名"/>
+      <FormItem :value="currentTag.name" @update:value="update" field-name="标签名：" placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
       <Button class="delete-button" @click="remove">删除标签</Button>
@@ -24,7 +24,7 @@ import Button from '@/components/Button.vue';
   components: {Button, FormItem},
 })
 export default class EditLabel extends Vue {
-  get tag() {
+  get currentTag() {
     return this.$store.state.currentTag;
   }
 
@@ -32,20 +32,20 @@ export default class EditLabel extends Vue {
     const id = this.$route.params.id;
     this.$store.commit('fetchTags');
     this.$store.commit('setCurrentTag', id);
-    if (!this.tag) {
+    if (!this.currentTag) {
       this.$router.replace('/404');
     }
   }
 
   update(name: string) {
-    if (this.tag) {
-      this.$store.commit('updateTag', {id: this.tag.id, name: name});
+    if (this.currentTag) {
+      this.$store.commit('updateTag', {id: this.currentTag.id, name: name});
     }
   }
 
   remove() {
-    if (this.tag) {
-      this.$store.commit('removeTag', this.tag.id);
+    if (this.currentTag) {
+      this.$store.commit('removeTag', this.currentTag.id);
     }
   }
 
